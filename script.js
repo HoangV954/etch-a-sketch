8ad1477b731c7dc1b78b7e4efebfdc1a91792a44
HoangV954 <hoangvu11793@gmail.com>
2022-11-24 23:18:13 +0700

    Finish the game
    Add buttons and their functions
    Revise game modes
    Add option to reset the board
    Fix board creating function
    Fix issues with color opacity and intensity increments

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 // Create the game board with certain number of boxes. Need a variable as max (for i <=) and utilize i as a separate variable since not just the width and height need to be changed
 const container = document.querySelector('.container');
-let size = 1;
+let size;
 
-function userSelection() {
-    return prompt('Choose your board size')
+function clear() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild)
+    }
 }
 
-function createBoard() {
-    size = false;//Number(userSelection());
-    if (!size) {
-        for (let i = 1; i <= 256; i++) {
-            const div = document.createElement('div');
-            div.classList.add('grid');
-            div.setAttribute('style', `width: calc(100%/16); height: calc(100%/16); border: solid 1px #696969; background-color: white;`);
-            container.appendChild(div);
-        } 
+function userSelection() {
+    return prompt('Choose your board size (maximum 100)')
+}
 
+//default mode
+function defaultBoard() {
+    for (let i = 1; i <= 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        div.setAttribute('style', `width: calc(100%/16); height: calc(100%/16); border: solid 1px #696969; background-color: white;`);
+        container.appendChild(div);
     }
+}
+defaultBoard()
 
-    if (size > 100) {
-        console.log('ERROR')
-    } else if (size <= 100 && size > 0) {
-        let max = size**2;
+
+// Utilize parseInt instead of Number() since Number tries to parse the entire argument as a number (fail when 1 of the character is NaN) while parseInt at least supports edge cases like ('12ab'). 2 methods: isNan and regex to check if user input is a number. regex is preferred since isNaN(parseInt(size)) is bad with white spaces (e.g. '  12' still counts as valid) 
+
+let regex = /^[0-9]+$/
+function createBoard() {
+    size = userSelection();
+    if (parseInt(size) > 100 || !regex.test(size)) {
+        return alert('Wrong input! Choose a number from 0 - 100!')
+    } else if (0 < parseInt(size) <= 100) {
+        clear();
+        let max = parseInt(size)**2;
         for (let i = 1; i <= max; i++) {
             const div = document.createElement('div');
             div.classList.add('grid');
-            div.setAttribute('style', `width: calc(100%/${size}); height: calc(100%/${size}); border: solid 1px #696969; background-color: white;`);
+            div.setAttribute('style', `width: calc(100%/${parseInt(size)}); height: calc(100%/${parseInt(size)}); border: solid 1px #696969; background-color: white;`);
             container.appendChild(div);
         } 
         
     }
 }
-
-createBoard()
 
 // Create random color variable. #ffffff is hexadecimal (base 16) compared to (255,255,255) <can't be inject directly into js> (base 10 - decimal). Sometimes the random math will return less than the standard length (e.g.: #00FF00) so it needs to be patched with a string on top of it (or refer to solution 2). Also, the number of colors that can be represented by this system is 16^6 or 256^3 or 2^24 = 16,777,216 (see sol. 2 for more usage of this info). What we learned here is it's good to research the underlying mechanics of the web before solving a logic puzzle.
 
@@ -42,8 +52,6 @@ createBoard()
 let color = '';
 let percent = 150;
 
-
-
 function randomColor() {
     let x = Math.round(0xffffc0*Math.random()).toString(16);
     let y = 6 - x.length;
@@ -51,9 +59,6 @@ function randomColor() {
     let z1 = z.substring(0, y);
     color = '#' + z1 + x;  
 }
-
-const grids = document.querySelectorAll('.grid');
-
 
 //Do not run this function with extensions that change color props. Also filter: brightness(20%); will make darker color. Anonymous function can be removed like below
 /*  if (percent < 0) {   
@@ -71,9 +76,10 @@ this.style.backgroundColor = color;
 let dark = 'brightness(' + percent + '%)';               
 this.style.filter = dark;  */
 
-
 //Revised version of the colorful mode
+
 function colorfulMode() {
+    const grids = document.querySelectorAll('.grid');
     grids.forEach((grid) => {
         let percent;   
         grid.addEventListener('mouseenter', (e) => {
@@ -91,28 +97,52 @@ function colorfulMode() {
     })
 }
 
+
 //Set a base number of items, increment with each time mouse hovers over. Utilizing black and white opacity vs rgba: rgba doesn't apply to border (opacity affects the whole element)
 
-function classicMode() {
+function classicMode() {  
+    const grids = document.querySelectorAll('.grid'); 
     grids.forEach((grid) => {
         let rgb = '(112, 112, 112,'      
-        grid.count = 0;
+        grid.count = 0.1;
         grid.addEventListener('mouseenter', (e) => {
-            e.target.count += 0.1;
+            e.target.count += 0.15;
             let rgba = 'rgba' + rgb + ' ' + e.target.count.toString() +')';
-            e.target.style.backgroundColor = 'red';
+            e.target.style.backgroundColor = rgba;
             //e.target.style.opacity = 0.5*e.target.count;
-            console.log(e.target.style)   
+               
         }, false)
     })    
 }
     
-colorfulMode()
 
 function reset() {
-    div.style.backgroundColor = 'white'
-    createBoard()
-    colorfulMode()
-}
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach((grid) => {
+        grid.style.backgroundColor = 'white'
+        grid.style.filter = 'brightness(100%)'
+    })
+} 
 
-reset()
+const btn = document.querySelectorAll('button');
+    btn.forEach((button) => {
+        if (button.classList.contains('erase')) {
+            button.addEventListener('click',reset)
+        }
+        
+        if (button.classList.contains('classic')) {
+            button.addEventListener('click',classicMode)
+        } 
+        
+        if (button.classList.contains('color')) {
+            button.addEventListener('click',colorfulMode)
+        }
+
+        if (button.classList.contains('size')) {
+            button.addEventListener('click', createBoard)
+        }
+    })
+
+
+
+
